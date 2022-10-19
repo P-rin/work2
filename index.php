2087f5f46e7ba5465866fbe1849342b10b1471e0
prin <actprin@gmail.com>
2022-10-19 12:07:42 +0700

    work6

--- index.php
+++ index.php
@@ -11,53 +11,69 @@
         .left{text-align: left;}
         .right{text-align: right;}
     </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <?php 
 if(!isset($_SESSION['id'])){
 ?>
 <body>
+    <div class="contianer">
     <h1 class="center"> web prin</h1>
+    <?php include "nav.php"; ?>
     <hr>
-    <form>หมวดหมู่:<select name="menu">
-        <option value="all">--ทั้งหมด--</option>
-        <option value="nomal">เรื่องทั่วไป</option>
-        <option value="study">เรื่องเรียน</option>
-        </select>
-        <a href="login.php" style="float: right;"><ins>เข้าสู่ระบบ</ins></a></form>
-        <a href="newpost.php"><ins>สร้างกระทู้ใหม่</ins></a>
+    <div class="d-flex">
+        <div>
+            <label>หมวดหมู่</label>
+            <span class="dropdown">
+                <button class="btn btn-light dropdown-toggle btn-sm" type="button" id="button2" data-bs-toggle="dropdown" aria-expanded="false">
+                    --ทั้งหมด--
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="button">
+                    <li><a href="#">ทั้งหมด</a></li>
+                    <li><a href="#">เรื่องเรียน</a></li>
+                    <li><a href="#">เรื่องทั่วไป</a></li>
+                </ul>
+            </span>
+        </div>
+    </div>
         <br>
-        <ul>
+        <table class="table table-striped">
         <?php for($i=1;$i<=10;$i++){
-            echo "<li><a href=post.php?id=$i>กระทู้$i</a></li>";
+            echo "<tr><td><a href=post.php?id=$i style=text-decoration:none>กระทู้$i</a></td></tr>";
             }
         ?>
-        </ul>
+        </table>
+    </div>
         </body>
         <?php } else{ ?>
                 <body>
-                <h1 class="center"> web prin</h1>
-                <hr>
-                <form>หมวดหมู่:<select name="menu">
-                <option value="all">--ทั้งหมด--</option>
-                <option value="nomal">เรื่องทั่วไป</option>
-                <option value="study">เรื่องเรียน</option>
-                </select>
-                <div style="float:right;"><?php 
-                echo "ผู้ใช้งานระบบ : $_SESSION[username] " ?>&nbsp;&nbsp;
-                <a href="logout.php"><ins>ออกจากระบบ</ins></a></form></div>
-                <a href="newpost.php"><ins>สร้างกระทู้ใหม่</ins></a>
-                <br>
-                <ul>
-                <?php for($i=1;$i<=10;$i++){
-                    if($_SESSION['role']=='a'){
-                        echo "<li><a href=post.php?id=$i>กระทู้$i</a> <a href=delete.php?id=$i> ลบ</a></li>";
+                <div class="contianer">
+                    <h1 class="center"> web prin</h1>
+                    <?php include "nav.php"; ?>
+                    <hr>
+                    <form>หมวดหมู่:<select name="menu">
+                    <option value="all">--ทั้งหมด--</option>
+                    <option value="nomal">เรื่องทั่วไป</option>
+                    <option value="study">เรื่องเรียน</option>
+                    </select>
+                    <a href="newpost.php"><ins>สร้างกระทู้ใหม่</ins></a>
+                    <br>
+                    <ul>
+                    <?php for($i=1;$i<=10;$i++){
+                        if($_SESSION['role']=='a'){
+                            echo "<li><a href=post.php?id=$i>กระทู้$i</a> <a href=delete.php?id=$i> ลบ</a></li>";
+                        }
+                        else { 
+                            echo "<li><a href=post.php?id=$i>กระทู้$i</a></li>";
+                        }
                     }
-                    else { 
-                        echo "<li><a href=post.php?id=$i>กระทู้$i</a></li>";
-                    }
-                }
-        ?>
-        </ul>
+                ?>
+            </ul>
+        </div>
         </body>
         <?php } ?>
 </html>
